de05f5118c2ec1ff57de19ecbfefb678bc29b4a7
NazarKravchenk0 <113011986+NazarKravchenk0@users.noreply.github.com>
2026-02-04 05:33:42 +0200

    Update apps.py

--- kitchen/apps.py
+++ kitchen/apps.py
@@ -2,5 +2,21 @@ from django.apps import AppConfig
 
 
 class KitchenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kitchen'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "kitchen"
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        from django.db.utils import OperationalError
+
+        User = get_user_model()
+
+        try:
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    password="admin12345",
+                )
+                print("✅ Test superuser created: admin / admin12345")
+        except OperationalError:
+            pass
